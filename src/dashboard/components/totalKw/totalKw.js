d168202edc7ee9d8e715838f6ffe44aa98aac39d
Tonrike <tornike@hostelpro.com>
2019-07-13 01:15:59 +0200

    feat(total-energy): created component

---
+++ src/dashboard/components/totalKw/totalKw.js
@@ -0,0 +1,31 @@
+import React     from "react";
+import PropTypes from "prop-types";
+import "./styles.scss";
+
+import { TOTAL_KW } from "../../../static/constants/constants";
+
+const totalKw = props => {
+
+	const total = props.total + TOTAL_KW.kw;
+
+	return (
+		<div className = "total-kw">
+
+			{/*header*/ }
+			<div className = "total-kw__header">
+				{ TOTAL_KW.totalEnergy }
+			</div>
+
+			{/*info number*/ }
+			<div className = "total-kw__info">
+				{ total }
+			</div>
+		</div>
+	);
+};
+
+totalKw.propTypes = {
+	total: PropTypes.number
+};
+
+export default totalKw;
--- src/static/constants/constants.js
+++ src/static/constants/constants.js
@@ -2,3 +2,8 @@ export const HEADER = {
 	userGuide: "user guide",
 	logout:    "logout"
 };
+
+export const TOTAL_KW = {
+	totalEnergy: "total energy",
+	kw:          "kw"
+};
